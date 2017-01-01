9f21bf81f57ddf1dbb02347e320017cef1334d62
borisb7 <borja993@mail.ru>
2017-01-01 21:36:28 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,130 +1,127 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Ensage;
-using Ensage.Common;
-using SharpDX;
-using SharpDX.Direct3D9;
-using Ensage.Common.Extensions;
-using Pudge_Plus.Classes;
-using System.Diagnostics;
-using System.Net;
-using System.Net.NetworkInformation;
-using System.Globalization;
-using System.IO;
-
-namespace Pudge_Plus
-{
-    class Program
-    {
-        
-        static void Main(string[] args)
-        {
-            
-            GlobalClasses.MakeConfig();
-            
-           // var whatthefuckamIdoing = new Variables.CustomInteger(ref Variables.Settings.Basic_ESP_Value);
-           
-            ESP.Draw.Interface.Add("Combo Status", ref Variables.Settings.Combo_Status_Value, "Curent Lethality of Combo", 0, 1, Variables.Settings.OnOff);
-            ESP.Draw.Interface.Add("Maximum Damage Output", ref Variables.Settings.Maximum_Damage_Output_Value,"Maximum Available Damage", 0, 1, Variables.Settings.OnOff);
-            ESP.Draw.Interface.Add("Auto-Hook", ref Variables.Settings.Auto_Hook_Value,"Press 'e' To Hook", 0, 1, Variables.Settings.OnOff);
-            ESP.Draw.Interface.Add("Auto-Combo", ref Variables.Settings.Auto_Combo_Value, "Auto execute combos", 0, 1, Variables.Settings.OnOff);
-            ESP.Draw.Interface.Add("Prediction Box", ref Variables.Settings.Prediction_Box_Value,"Predicted location of enemy", 0, 1, Variables.Settings.OnOff);
-         
-          
-           
-           
-            
-            
-            Game.OnWndProc += Game_OnWndProc; //Keystroke Reader
-            Drawing.OnDraw += Drawing_OnDraw; //Graphical Drawer
-         
-            
-        }
-
-
-         
-
-            //Get players
-            var players = ESP.Calculate.SpecificLists.GetPlayersNoSpecsNoIllusionsNoNull(); //Get Players
-            List<Player> pla = players;
-            if (!players.Any())
-                return;
-
-            //Reset runes after waiting time
-            
-
-
-            
-        
-
-
-
-
-            Variables.Settings.Skill_Shot_Notifier_Value.val = 0;
-            
-            if (Variables.Settings.Skill_Shot_Notifier_Value.val == 0)
-                ESP.Draw.Enemy.SkillShotDisplay(); //Draw global skill shots
-            Variables.EnemyIndex = 0;
-            int enemyIndex = 0;
-            foreach (var enemy in ESP.Calculate.SpecificLists.EnemyHeroNotIllusion(players))
-            {
-                if (enemy.Player.Hero.IsAlive && enemy.Player.Hero.IsVisible)
-                {
-                    Variables.Settings.Enemy_Tracker_Value.val = 0;
-                    if (Variables.Settings.Enemy_Tracker_Value.val == 0)
-                    {
-                        Variables.EnemyTracker[enemyIndex].EnemyTracker = enemy;
-                        Variables.EnemyTracker[enemyIndex].RelativeGameTime = (int)Game.GameTime;
-                    }
-                    
-                    if (enemy.Distance2D(ObjectMgr.LocalHero) <= 2000)
-                    {
-                        ESP.Draw.Enemy.basic(enemy);
-                        if (Variables.me.Name == "npc_dota_hero_pudge")
-                        {
-                            HookHandler.main(enemy);
-                            ESP.Draw.Enemy.pudge(enemy);
-                            if (ESP.Calculate.Enemy.isMoving(enemy.Position, Variables.EnemyIndex))
-                            {
-                                try
-                                {
-                                    if (Variables.Settings.Prediction_Box_Value.val == 0)
-                                    {
-                                        HookHandler.PredictClass predict = HookHandler.getPrediction(Variables.me, enemy, Variables.PredictMethod);
-                                        if (predict.PredictedLocation != Vector2.Zero)
-                                            ESP.Draw.Enemy.PredictionBox(predict, Color.Red);
-                                    }
-                                }
-                                catch { }
-                            }
-                            else
-                            {
-                                if (Variables.Settings.Auto_Hook_Value.val == 0)
-                                {
-                                    var closest = ESP.Calculate.Enemy.ClosestToMouse(Variables.me, 1400);
-                                    if (closest != null && closest.Player.Name == enemy.Player.Name)
-                                    {
-                                        ESP.Draw.Enemy.Info(enemy, "Locked [e]", 5, Color.DarkOrange, FontFlags.Outline | FontFlags.AntiAlias);
-                                        if (Variables.HookForMe && Utils.SleepCheck("hook"))
-                                        {
-                                            Variables.me.Spellbook.SpellQ.UseAbility(enemy.Position);
-                                           // Print.Info(enemy.Name);
-                                            Print.Info("Hooking for you.");
-                                            Utils.Sleep(1000, "hook");
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    Variables.EnemiesPos[Variables.EnemyIndex] = enemy.Position;
-                    Variables.EnemyIndex++;
-                }
-                
-            }
-        }
-    }
-}
+<?xml version="1.0" encoding="utf-8"?>
+<Project ToolsVersion="14.0" DefaultTargets="Build" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+  <Import Project="$(MSBuildExtensionsPath)\$(MSBuildToolsVersion)\Microsoft.Common.props" Condition="Exists('$(MSBuildExtensionsPath)\$(MSBuildToolsVersion)\Microsoft.Common.props')" />
+  <PropertyGroup>
+    <Configuration Condition=" '$(Configuration)' == '' ">Release</Configuration>
+    <Platform Condition=" '$(Platform)' == '' ">AnyCPU</Platform>
+    <ProjectGuid>{B6E3519B-C439-4482-AF6F-24B84D34B58B}</ProjectGuid>
+    <OutputType>Exe</OutputType>
+    <AppDesignerFolder>Properties</AppDesignerFolder>
+    <RootNamespace>Pudge_Plus</RootNamespace>
+    <AssemblyName>Pudge Plus</AssemblyName>
+    <TargetFrameworkVersion>v4.5.2</TargetFrameworkVersion>
+    <FileAlignment>512</FileAlignment>
+    <AutoGenerateBindingRedirects>true</AutoGenerateBindingRedirects>
+    <PreBuildEvent />
+    <PostBuildEvent />
+    <OutputPath>bin\Release</OutputPath>
+    <PublishUrl>publish\</PublishUrl>
+    <Install>true</Install>
+    <InstallFrom>Disk</InstallFrom>
+    <UpdateEnabled>false</UpdateEnabled>
+    <UpdateMode>Foreground</UpdateMode>
+    <UpdateInterval>7</UpdateInterval>
+    <UpdateIntervalUnits>Days</UpdateIntervalUnits>
+    <UpdatePeriodically>false</UpdatePeriodically>
+    <UpdateRequired>false</UpdateRequired>
+    <MapFileExtensions>true</MapFileExtensions>
+    <ApplicationRevision>0</ApplicationRevision>
+    <ApplicationVersion>1.0.0.%2a</ApplicationVersion>
+    <IsWebBootstrapper>false</IsWebBootstrapper>
+    <UseApplicationTrust>false</UseApplicationTrust>
+    <BootstrapperEnabled>true</BootstrapperEnabled>
+  </PropertyGroup>
+  <PropertyGroup Condition=" '$(Configuration)|$(Platform)' == 'Debug|AnyCPU' ">
+    <PlatformTarget>x64</PlatformTarget>
+    <DebugSymbols>true</DebugSymbols>
+    <DebugType>full</DebugType>
+    <Optimize>false</Optimize>
+    <OutputPath>bin\Release</OutputPath>
+    <DefineConstants>DEBUG;TRACE</DefineConstants>
+    <ErrorReport>prompt</ErrorReport>
+    <WarningLevel>4</WarningLevel>
+  </PropertyGroup>
+  <PropertyGroup Condition=" '$(Configuration)|$(Platform)' == 'Release|AnyCPU' ">
+    <PlatformTarget>x64</PlatformTarget>
+    <DebugType>pdbonly</DebugType>
+    <Optimize>true</Optimize>
+    <OutputPath>bin\Release</OutputPath>
+    <DefineConstants>TRACE</DefineConstants>
+    <ErrorReport>prompt</ErrorReport>
+    <WarningLevel>4</WarningLevel>
+  </PropertyGroup>
+  <ItemGroup>
+    <Reference Include="Ensage, Version=0.0.0.1, Culture=neutral, PublicKeyToken=a41ba14f653257a6, processorArchitecture=AMD64">
+      <SpecificVersion>False</SpecificVersion>
+      <HintPath>C:\Users\Daniel\Downloads\LeagueSharp\System\Ensage.dll</HintPath>
+    </Reference>
+    <Reference Include="Ensage.Common, Version=1.1.0.0, Culture=neutral, processorArchitecture=AMD64">
+      <SpecificVersion>False</SpecificVersion>
+      <HintPath>C:\Users\Daniel\Downloads\LeagueSharp\System\Ensage.Common.dll</HintPath>
+    </Reference>
+    <Reference Include="SharpDX, Version=2.6.3.0, Culture=neutral, PublicKeyToken=345b829e6e492981, processorArchitecture=MSIL">
+      <SpecificVersion>False</SpecificVersion>
+      <HintPath>C:\Users\Daniel\Downloads\LeagueSharp\System\SharpDX.dll</HintPath>
+    </Reference>
+    <Reference Include="SharpDX.Direct3D9, Version=2.6.3.0, Culture=neutral, processorArchitecture=MSIL">
+      <SpecificVersion>False</SpecificVersion>
+      <HintPath>C:\Users\Daniel\Downloads\LeagueSharp\System\SharpDX.Direct3D9.dll</HintPath>
+    </Reference>
+    <Reference Include="System" />
+    <Reference Include="System.Core" />
+    <Reference Include="System.Drawing" />
+    <Reference Include="System.Xml.Linq" />
+    <Reference Include="System.Data.DataSetExtensions" />
+    <Reference Include="Microsoft.CSharp" />
+    <Reference Include="System.Data" />
+    <Reference Include="System.Net.Http" />
+    <Reference Include="System.Xml" />
+  </ItemGroup>
+  <ItemGroup>
+    <Compile Include="Classes\ESP.cs" />
+    <Compile Include="Classes\GlobalClasses.cs" />
+    <Compile Include="Classes\HookHandler.cs" />
+    <Compile Include="Classes\Print.cs" />
+    <Compile Include="Classes\RuneHandler.cs" />
+    <Compile Include="Classes\Variables.cs" />
+    <Compile Include="Program.cs" />
+    <Compile Include="Properties\AssemblyInfo.cs" />
+    <Compile Include="Properties\Settings.Designer.cs">
+      <AutoGen>True</AutoGen>
+      <DesignTimeSharedInput>True</DesignTimeSharedInput>
+      <DependentUpon>Settings.settings</DependentUpon>
+    </Compile>
+  </ItemGroup>
+  <ItemGroup>
+    <None Include="App.config">
+      <SubType>Designer</SubType>
+    </None>
+    <None Include="Properties\Settings.settings">
+      <Generator>SettingsSingleFileGenerator</Generator>
+      <LastGenOutput>Settings.Designer.cs</LastGenOutput>
+    </None>
+  </ItemGroup>
+  <ItemGroup>
+    <Analyzer Include="..\..\..\..\..\Downloads\sharpdx\Bin\DirectX11_2-net40\SharpDX.Direct3D9.dll" />
+  </ItemGroup>
+  <ItemGroup>
+    <BootstrapperPackage Include=".NETFramework,Version=v4.5.2">
+      <Visible>False</Visible>
+      <ProductName>Microsoft .NET Framework 4.5.2 %28x86 and x64%29</ProductName>
+      <Install>true</Install>
+    </BootstrapperPackage>
+    <BootstrapperPackage Include="Microsoft.Net.Framework.3.5.SP1">
+      <Visible>False</Visible>
+      <ProductName>.NET Framework 3.5 SP1</ProductName>
+      <Install>false</Install>
+    </BootstrapperPackage>
+  </ItemGroup>
+  <Import Project="$(MSBuildToolsPath)\Microsoft.CSharp.targets" />
+  <!-- To modify your build process, add your task inside one of the targets below and uncomment it. 
+       Other similar extension points exist, see Microsoft.Common.targets.
+  <Target Name="BeforeBuild">
+  </Target>
+  <Target Name="AfterBuild">
+  </Target>
+  -->
+</Project>
